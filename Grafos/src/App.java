57e89f16817e02b364493130ec7915bdf12577bc
Sara Iglesias <sara.iglesias0309@gmail.com>
2022-04-24 22:04:39 -0300

    Distâncias ordenadas

--- Grafos/src/App.java
+++ Grafos/src/App.java
@@ -28,10 +28,40 @@ public class App {
 
         f.fecharArq();
 
-        for (int i = 0; i < quant; i++) {
-            System.out.println(i);
-            System.out.println(cidades[i].getNome() + " " + cidades[i].getLatitude() + " " + cidades[i].getLongitude());
-        }
+        FileWriter arq = new FileWriter("distancias.txt");
+        PrintWriter gravarArq = new PrintWriter(arq);
 
+        Distancias distancias[];
+
+        for (int i = 0; i < (cidades.length); i++) {
+            gravarArq.printf("Id cidade: " + i + " | Nome da cidade: "
+                    + cidades[i].getNome());
+            for (int j = 0; j < (cidades.length); j++) {
+                gravarArq.printf("\nDistancia entre as cidades " + cidades[i].getNome() + " e " + cidades[j].getNome()
+                        + " é " + cidades[i].distancia(cidades[j]));
+                distancias = new Distancias(cidades[i].getNome(), cidades[j].getNome(), cidades[i].distancia(cidades[j]));
+            }
+            gravarArq.printf("\n\n");
+        }
+        arq.close();
+
+        Distancia distanciaOrd[];
+
+        distanciaOrd = distancia;
+
+        Collections.sort(distanciaOrd, new Comparable<Distancia>() {
+            @Override
+            public int compare(Object cid1, Object cid2) {
+                if(cid1.compareTo(cid2) == 1) {
+                    return 1;
+                } else if (cid1.compareTo(cid2) == -1) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
+
+        
+        
     }
 }
---
+++ Grafos/src/Distancias.java
@@ -0,0 +1,26 @@
+public class Distancias implements Comparable{
+    private String cidade1;
+    private String cidade2;
+    private double distancia;
+
+    public Distancias(String cidade1, String cidade2, double distancia) {
+        this.cidade1 = cidade1;
+        this.cidade2 = cidade2;
+        this.distancia = distancia;
+    }
+
+
+    @Override
+    public int CompareTo(Object outra) {
+        if(this.distancia < outra.getDistancia()) {
+            return 1;
+        } else if(this.distancia > outra.getDistancia()) {
+            return -1;
+        }
+        return 0;
+    }
+
+    public double getDistancia() {
+        return this.distancia;
+    }
+}
